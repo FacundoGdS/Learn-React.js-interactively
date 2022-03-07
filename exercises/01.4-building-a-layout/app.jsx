a1ecb7d40bac72459cd98f41fc13f8649872414c
Facundo <facundogds@gmail.com>
2022-03-07 23:31:03 +0000

    Entre 01.4 y 01.5

--- exercises/01.4-building-a-layout/app.jsx
+++ exercises/01.4-building-a-layout/app.jsx
@@ -13,12 +13,12 @@ const data = {
 };
 
 let content = (
-	<div className="card" style={{ width: "18rem" }}>
-		<img className="card-img-top" src={data.image} alt="Card image cap" />
+	<div className="card m-5">
+		<img alt="Card image cap" className="card-img-top" src={data.image} alt="Card image cap" />
 		<div className="card-body">
-			<h5 className="card-title">{data.cardTitle} </h5>
-			<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-			<a href="#" className="btn btn-primary">
+			<h5 className="card-title">{data.cardTitle}</h5>
+			<p className="card-text">{data.cardDescription}</p>
+			<a href={data.button.url} className="btn btn-primary">
 				{data.button.label}
 			</a>
 		</div>
